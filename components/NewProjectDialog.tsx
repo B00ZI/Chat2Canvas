eec7f785834dbee12adc196e5bf5cd7decf02ada
B00ZI <iraamaneyahya@gmail.com>
2026-02-02 14:34:39 +0100

    moving the addprojict logic to modale

--- components/NewProjectDialog.tsx
+++ components/NewProjectDialog.tsx
@@ -1,31 +1,50 @@
-
+import { useRef } from "react"
 import {
     Dialog,
     DialogContent,
     DialogDescription,
     DialogHeader,
     DialogTitle,
-    DialogTrigger,
-}
-    from "@/components/ui/dialog"
+    
+} from "@/components/ui/dialog"
+import { Button } from "./ui/button"
+import { Input } from "./ui/input"
+import { useProjectStore } from "@/store/projectStore"
 interface NewProjectDialog {
     open: boolean
     onClose: () => void
 }
 
-export function NewProjectDialog({open , onClose}:NewProjectDialog) {
+
+export function NewProjectDialog({ open, onClose }: NewProjectDialog) {
+
+    const inputRef = useRef<HTMLInputElement>(null)
+    const addProject = useProjectStore((state) => state.addProject)
+   
+
+
+    function addNewProject() {
+        let name = inputRef.current?.value
+        if (name) {
+            addProject(name)
+        }
+        onClose()
+    }
 
     return (
 
         <Dialog open={open} onOpenChange={onClose}>
-           
+
             <DialogContent>
                 <DialogHeader>
-                    <DialogTitle>create New Project</DialogTitle>
+                    <DialogTitle>Create New Project</DialogTitle>
                     <DialogDescription>
-                        provide your 
+                        provide your
                     </DialogDescription>
                 </DialogHeader>
+
+                <Input  type="text"  ref={inputRef}/>
+                <Button onClick={addNewProject}>Create</Button>
             </DialogContent>
         </Dialog>
     )
