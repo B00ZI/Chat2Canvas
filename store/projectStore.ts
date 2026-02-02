fde5b39de0b7b3dd008593ba8e220ed2d233b595
B00ZI <iraamaneyahya@gmail.com>
2026-02-02 22:26:27 +0100

    add edite functionalty and delete in projicts store

--- store/projectStore.ts
+++ store/projectStore.ts
@@ -1,47 +1,60 @@
-import { create } from 'zustand'
+import { create } from "zustand";
 
 interface Project {
-  id: string  
-  name: string
+  id: string;
+  name: string;
 }
 
 interface ProjectStore {
-  projects: Project[]  
-  activeProjectId: string| null  
-  
-  addProject: (name : string) => void  
-  deleteProject: (id : string) => void
-  setActiveProject: (id:string) => void
+  projects: Project[];
+  activeProjectId: string | null;
+
+  addProject: (name: string) => void;
+  editProject: (id: string, newName: string) => void;
+  deleteProject: (id: string) => void;
+  setActiveProject: (id: string) => void;
 }
 
 export const useProjectStore = create<ProjectStore>((set) => ({
   // Initial data values
   projects: [],
   activeProjectId: null,
-  
+
   // Functions (actions)
   addProject: (name) => {
-    
     const newProject = {
-        id:String(Date.now()),
-        name:name
-    }
+      id: String(Date.now()),
+      name: name,
+    };
 
     set((state) => ({
-       projects : [ ...state.projects , newProject ],
-       activeProjectId: newProject.id
-    }))
-    
-   
+      projects: [...state.projects, newProject],
+      activeProjectId: newProject.id,
+    }));
   },
-  
+
+  editProject: (id, newName) => {
+    set((state) => ({
+      projects: state.projects.map((project) =>
+        project.id === id ? { ...project, name: newName } : project,
+      ),
+    }));
+  },
+
   deleteProject: (id) => {
-     set((state)=>({
-        projects: state.projects.filter((project)=> project.id !== id)
-     }))
+    set((state) => {
+      const newProjects = state.projects.filter((project) => project.id !== id);
+      return {
+        projects: newProjects,
+        activeProjectId:
+          state.activeProjectId === id
+            ? newProjects[0]?.id || null 
+            : state.activeProjectId,
+      };
+    });
   },
-  
+
   setActiveProject: (id) => {
-    set({activeProjectId : id})
-  }
-}))
+    set({ activeProjectId: id });
+  },
+}));
