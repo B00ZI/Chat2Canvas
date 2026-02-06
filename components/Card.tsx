f99918c4d0c51a85ad82d397a5cc8dfba46165ef
B00ZI <iraamaneyahya@gmail.com>
2026-02-06 23:37:24 +0100

    make card to expect full card prop and use togel task in it

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,12 +1,25 @@
-interface CardProps {
+import { useProjectStore } from "@/store/projectStore";
+
+interface card {
+  id: string 
   number: number
   title: string
   color: string
   tasks: { text: string; done: boolean }[]
 }
 
-export default function Card({ number, title, color, tasks }: CardProps) {
-  const completedTasks = tasks.filter(t => t.done).length
+interface CardPrps {
+  
+  card: card ,
+  projectId: string,
+  colId: string,
+ 
+}
+
+export default function Card({ card , projectId , colId }: CardPrps) {
+
+  const toggleTask = useProjectStore((state) => state.toggleTask)
+  const completedTasks = card.tasks.filter(t => t.done).length
 
   return (
     <div className="bg-white border border-gray-200 rounded-lg p-4 hover:shadow-md transition-shadow cursor-pointer">
@@ -15,39 +28,40 @@ export default function Card({ number, title, color, tasks }: CardProps) {
         {/* Number Badge */}
         <div 
           className="w-8 h-8 rounded-full flex items-center justify-center text-white font-semibold text-sm shrink-0"
-          style={{ backgroundColor: color }}
+          style={{ backgroundColor: card.color }}
         >
-          {number}
+          {card.number}
         </div>
         
         {/* Title */}
-        <h4 className="font-semibold text-gray-900 flex-1">{title}</h4>
+        <h4 className="font-semibold text-gray-900 flex-1">{card.title}</h4>
       </div>
 
       {/* Tasks Preview */}
       <div className="space-y-2">
-        {tasks.slice(0, 3).map((task, idx) => (
+        {card.tasks.slice(0, 3).map((task, idx) => (
           <div key={idx} className="flex items-center gap-2 text-sm">
             <input 
+              onChange={()=> toggleTask(projectId , colId , card.id ,  idx)}
               type="checkbox" 
               checked={task.done}
               className="rounded"
-              readOnly
+              
             />
             <span className={task.done ? "line-through text-gray-400" : "text-gray-700"}>
               {task.text}
             </span>
           </div>
         ))}
-        {tasks.length > 3 && (
-          <p className="text-xs text-gray-500">+{tasks.length - 3} more tasks</p>
+        {card.tasks.length > 3 && (
+          <p className="text-xs text-gray-500">+{card.tasks.length - 3} more tasks</p>
         )}
       </div>
 
       {/* Footer */}
       <div className="mt-3 pt-3 border-t border-gray-100 flex items-center justify-between">
         <span className="text-xs text-gray-500">
-          {completedTasks}/{tasks.length} completed
+          {completedTasks}/{card.tasks.length} completed
         </span>
       </div>
     </div>
