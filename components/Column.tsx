e87b5ecf7470701c1be449dcabdd22c2e72a0e7f
B00ZI <iraamaneyahya@gmail.com>
2026-02-06 23:38:45 +0100

    make colnm to pass card and map and return card compnent for itach card

--- components/Column.tsx
+++ components/Column.tsx
@@ -2,41 +2,40 @@
 import { useState } from "react"
 import Card from "./Card"
 import { EditColumnDialog } from "./EditColumnDialog"
+import { NewCardDialog } from "./NewCardDialog "
 
-interface ColumnProps {
-    title: string
-    color: string
-    cardCount: number
-}
+
 
 interface Task {
-  text: string;
-  done: boolean;
+    text: string;
+    done: boolean;
 }
 
 interface Card {
-  id: string;
-  number: number;
-  title: string;
-  color: string;
-  tasks: Task[];
+    id: string;
+    number: number;
+    title: string;
+    color: string;
+    tasks: Task[];
 }
 
 interface Column {
-  id: string;
-  title: string;
-  color: string;
-  cards: Card[];
+    id: string;
+    title: string;
+    color: string;
+    cards: Card[];
 }
 
 interface ColumnProps {
-  col: Column;
-  projectId: string; // Add this!
+    col: Column;
+    projectId: string; // Add this!
 }
 
-export default function Column( {col , projectId} : ColumnProps) {
+export default function Column({ col, projectId }: ColumnProps) {
 
-const [isEditColumnDialogOpen , setIsEditColumnDialogOpen] = useState(false)
+
+    const [isEditColumnDialogOpen, setIsEditColumnDialogOpen] = useState(false)
+    const [isNewCardDialogOpen, setisNewCardDialogOpen] = useState(false)
     return (
         <div className="bg-white rounded-lg p-4 w-80 shrink-0 shadow-sm border border-gray-200">
             {/* Column Header */}
@@ -47,30 +46,34 @@ const [isEditColumnDialogOpen , setIsEditColumnDialogOpen] = useState(false)
                         <h3 className="font-semibold text-lg">{col.title}</h3>
                         {/* <p className="text-sm text-gray-500">{cardCoun} tasks</p> */}
                     </div>
-                    <button onClick={()=>setIsEditColumnDialogOpen(true)} className="text-gray-400 hover:text-gray-600">⋮</button>
+                    <button onClick={() => setIsEditColumnDialogOpen(true)} className="text-gray-400 hover:text-gray-600">⋮</button>
                     <EditColumnDialog open={isEditColumnDialogOpen} onClose={() => setIsEditColumnDialogOpen(false)} projectId={projectId} col={col} />
                 </div>
             </div>
-           
-            {/* Cards go here - empty for now */}
 
+            {/* Cards go here - empty for now */}
             <div className="space-y-3 mb-3">
-                <Card
-                    number={1}
-                    title="Setup Database"
-                    color= {col.color}
-                    tasks={[
-                        { text: "Create schema", done: true },
-                        { text: "Setup migrations", done: false },
-                        { text: "Add seed data", done: false }
-                    ]}
-                />
+                {col.cards.map((card, x) =>
+
+
+                    <Card
+                        key={card.id}
+                        card={card}
+                        projectId={projectId}
+                        colId={col.id}
+                    />
+
+                )}
             </div>
 
+
             {/* Add Card Button */}
-            <button className="w-full border-2 border-dashed border-gray-300 rounded-lg p-3 text-sm text-gray-500 hover:border-gray-400 hover:text-gray-600">
-                + Add New Task
+            <button onClick={() => setisNewCardDialogOpen(true)} className="w-full flex items-center justify-center gap-2 cursor-pointer border-2 border-dashed border-gray-200 rounded-lg p-3 text-sm font-medium text-gray-500 transition-colors hover:bg-gray-50 hover:border-gray-300! hover:text-gray-700">
+                <span className="text-lg">+</span>
+                Add New Card
             </button>
+            <NewCardDialog open={isNewCardDialogOpen} onClose={() => setisNewCardDialogOpen(false)} colId={col.id} projectId={projectId} />
+
         </div>
     )
 }
