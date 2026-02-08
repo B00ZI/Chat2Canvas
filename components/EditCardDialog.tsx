df248f39e5b17858a4d037873d597061ae4acb56
B00ZI <iraamaneyahya@gmail.com>
2026-02-08 21:57:52 +0100

    add the edite dialoge with crud functionality and use the dtore functions

---
+++ components/EditCardDialog.tsx
@@ -0,0 +1,193 @@
+import { useRef, useState, useEffect } from "react"
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+
+} from "@/components/ui/dialog"
+
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { Button } from "./ui/button"
+import { Input } from "./ui/input"
+import { useProjectStore } from "@/store/projectStore"
+
+
+interface Task {
+    text: string;
+    done: boolean;
+}
+
+interface Card {
+    id: string;
+    number: number;
+    title: string;
+    color: string;
+    tasks: Task[];
+}
+
+interface EditCardDialog {
+    open: boolean
+    onClose: () => void,
+    projectId: string,
+    colId: string,
+    card: Card
+}
+
+
+
+export function EditCardDialog({ open, onClose, projectId, colId, card }: EditCardDialog) {
+
+    const [tasks, setTasks] = useState<Task[]>([])
+
+    const titleInputRef = useRef<HTMLInputElement>(null)
+    const colorInputRef = useRef<HTMLInputElement>(null)
+    const taskInputRef = useRef<HTMLInputElement>(null)
+    const editCard = useProjectStore((state) => state.editCard)
+    const deleteCard = useProjectStore((state) => state.deleteCard)
+
+    useEffect(() => {
+        if (open) {
+            setTasks(card.tasks)
+        }
+    }, [card.id, open])
+
+    function handleAddTask() {
+
+        const taskText = taskInputRef.current?.value.trim()
+
+        if (taskText && taskInputRef.current) {
+            const newTask = { text: taskText, done: false }
+            setTasks((prev) => [...prev, newTask])
+            taskInputRef.current.value = ""
+
+        }
+
+
+
+    }
+
+    function handleDeleteTask(taskIndex: number) {
+
+        setTasks((prev) => prev.filter((_, index) => index !== taskIndex))
+
+    }
+
+    function handleSubmit(e: React.FormEvent) {
+        e.preventDefault(); // Prevents the page from reloading
+        let title = titleInputRef.current?.value.trim();
+        let color = colorInputRef.current?.value.trim();
+        if (title && color) {
+            editCard(projectId, colId, card.id, { title, color, tasks });
+            onClose()
+
+        }
+    }
+
+    function handleDelete() {
+
+        deleteCard(projectId, colId, card.id)
+        onClose()
+
+    }
+
+    return (
+
+        <Dialog open={open} onOpenChange={onClose}>
+
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Edit Card</DialogTitle>
+                    <DialogDescription>
+                        You can edit the Card Title and color and tasks
+                    </DialogDescription>
+                </DialogHeader>
+                <form className="space-y-4" onSubmit={handleSubmit}>
+                    <div className="flex flex-col gap-4">
+
+                        <label className="text-sm font-medium  ">Card Title</label>
+                        <Input placeholder="e.g., Frontend" type="text" ref={titleInputRef} defaultValue={card.title} />
+
+                        <label className="text-sm font-medium  ">Card Color</label>
+                        <Input type="color" ref={colorInputRef} defaultValue={card.color} />
+
+                        <label className="text-sm font-medium  ">Card Tasks</label>
+                        <div className="flex gap-3">
+
+                            <Input onKeyDown={(e) => e.key === 'Enter' && (e.preventDefault(), handleAddTask())} className="flex-3" type="text" ref={taskInputRef} />
+
+                            <button onClick={handleAddTask} type="button" className="w-full  flex flex-1 items-center justify-center gap-2 cursor-pointer border  border-gray-200 rounded-lg text-sm font-medium text-gray-500 transition-colors hover:bg-gray-50 hover:border-gray-300! hover:text-gray-700">
+                                <span className="text-lg">+</span>
+                                Add
+                            </button>
+                        </div>
+                        <div className="flex flex-col gap-2 overflow-y-auto max-h-70">
+                            {[...tasks].reverse().map((task, taskIndex) => (
+                                <div key={taskIndex} className="flex gap-2 items-center border p-2 rounded-lg bg-white group">
+                                    {/* Dynamic Task Input */}
+                                    <Input
+                                        value={task.text}
+                                        onChange={(e) => {
+                                            setTasks(prev => prev.map((task, i) =>
+                                                i === taskIndex ? { ...task, text: e.target.value } : task
+                                            ));
+                                        }}
+                                        className="flex-1 border-none bg-transparent focus-visible:ring-0 shadow-none p-0 h-auto font-medium text-gray-600"
+                                    />
+
+                                    <button
+                                        onClick={() => handleDeleteTask(taskIndex)}
+                                        type="button"
+                                        className="opacity-0 group-hover:opacity-100 text-xs text-red-500 hover:text-red-700 transition-all p-1"
+                                    >
+                                        Remove
+                                    </button>
+                                </div>
+                            ))}
+                        </div>
+
+                    </div>
+
+                    <div className="flex gap-2">
+                        <Button type="submit" className="flex-1">
+                            Save Changes
+                        </Button>
+
+                        <AlertDialog>
+                            <AlertDialogTrigger asChild>
+                                <Button variant="destructive" type="button" className="flex-1">
+                                    Delete
+                                </Button>
+                            </AlertDialogTrigger>
+                            <AlertDialogContent>
+                                <AlertDialogHeader>
+                                    <AlertDialogTitle>Delete "{card?.title}" Card ?</AlertDialogTitle>
+                                    <AlertDialogDescription>
+                                        This action cannot be undone. This will permanently delete this Card .
+                                    </AlertDialogDescription>
+                                </AlertDialogHeader>
+                                <AlertDialogFooter>
+                                    <AlertDialogCancel>Cancel</AlertDialogCancel>
+                                    <AlertDialogAction onClick={handleDelete}>
+                                        Delete Card
+                                    </AlertDialogAction>
+                                </AlertDialogFooter>
+                            </AlertDialogContent>
+                        </AlertDialog>
+                    </div>
+                </form>
+            </DialogContent>
+        </Dialog>
+    )
+}
