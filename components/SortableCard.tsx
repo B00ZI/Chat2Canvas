ee39f3d6769108f4aff7e483d809ea54c4b3d815
B00ZI <iraamaneyahya@gmail.com>
2026-02-09 14:59:20 +0100

    add a dynamic placeholder for cards while draging

--- components/SortableCard.tsx
+++ components/SortableCard.tsx
@@ -9,24 +9,34 @@ interface SortableCardProps {
 }
 
 export default function SortableCard({ card, projectId, colId }: SortableCardProps) {
-  const {
-    attributes,
-    listeners,
-    setNodeRef,
-    transform,
-    transition,
-    isDragging,
-  } = useSortable({ id: card.id })
+  const { setNodeRef, transform, transition, isDragging, attributes, listeners, node } =
+    useSortable({ id: card.id });
 
   const style = {
-    transform: CSS.Transform.toString(transform),
+    transform: CSS.Translate.toString(transform),
     transition,
-    opacity: isDragging ? 0.5 : 1,
+  };
+
+  if (isDragging) {
+    const height = node.current?.offsetHeight;
+    return (
+      <div
+        ref={setNodeRef}
+        style={{
+          ...style,
+          height: height ? `${height}px` : '150px',
+          backgroundColor: card.color,
+          borderColor: 'rgba(0,0,0,0.3)',
+         
+        }}
+       className="w-full rounded-lg border-2 opacity-50 shadow-inner"
+      />
+    );
   }
 
   return (
     <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
       <Card card={card} projectId={projectId} colId={colId} />
     </div>
-  )
+  );
 }
