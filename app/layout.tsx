4123caa3abfdd618ae3cd9e8f80b3fc0de39078b
B00ZI <iraamaneyahya@gmail.com>
2026-02-15 21:11:48 +0000

    instla them fonst in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Oxanium, Merriweather, Fira_Code } from "next/font/google";
 import "./globals.css";
 import Sidebar from "@/components/Sidebar";
-import Topbar from "@/components/Topbar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const oxanium = Oxanium({ subsets: ["latin"], variable: "--font-oxanium" });
+const merriweather = Merriweather({ weight: ["400", "700"], subsets: ["latin"], variable: "--font-merriweather" });
+const firaCode = Fira_Code({ subsets: ["latin"], variable: "--font-fira-code" });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,13 +18,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${oxanium.variable} ${merriweather.variable} ${firaCode.variable}`}>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        
+        className="font-sans antialiased">
+
         <div className="flex">
-         
+
           <Sidebar />
           <main className="flex-1 bg-gray-50">
             {children}
