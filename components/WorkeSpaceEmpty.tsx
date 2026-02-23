0200da5d2ad2274e20b841c20b6ac46ece7c57e4
B00ZI <iraamaneyahya@gmail.com>
2026-02-23 17:45:52 +0000

    add dialoges for creat project and canvas tools

--- components/WorkeSpaceEmpty.tsx
+++ components/WorkeSpaceEmpty.tsx
@@ -8,34 +8,52 @@ import {
   EmptyTitle,
 } from "@/components/ui/empty"
 import { Folder } from "lucide-react"
-
+import { useState } from "react"
+import AIToolsModal from "./AIToolsModal"
+import { NewProjectDialog } from "./NewProjectDialog"
 export function EmptyDemo() {
+
+  const [isToolsOpen, setIsToolsOpen] = useState(false)
+  const [isNewProjecOpen, setIsNewProjecOpen] = useState(false)
+
   return (
-    <Empty>
-      <EmptyHeader>
-        <EmptyMedia variant="icon">
-          <Folder className="text-muted-foreground" />
-        </EmptyMedia>
+    <>
+      <Empty>
+        <EmptyHeader>
+          <EmptyMedia variant="icon">
+            <Folder className="text-muted-foreground" />
+          </EmptyMedia>
 
-        <EmptyTitle>
-          No projects yet
-        </EmptyTitle>
+          <EmptyTitle>
+            No projects yet
+          </EmptyTitle>
 
-        <EmptyDescription>
-          Start a new project from scratch, or let the Canvas tools guide you
-          to use Ai to create one in seconds.
-        </EmptyDescription>
-      </EmptyHeader>
+          <EmptyDescription>
+            Start a new project from scratch, or let the Canvas tools guide you
+            to use Ai to create one in seconds.
+          </EmptyDescription>
+        </EmptyHeader>
 
-      <EmptyContent className="flex-row justify-center gap-2">
-        <Button>
-          Start with Canvas tools
-        </Button>
+        <EmptyContent className="flex-row justify-center gap-2">
+          <Button onClick={() => setIsToolsOpen(true)}>
+            Start with Canvas tools
+          </Button>
 
-        <Button variant="outline">
-          Create project manually
-        </Button>
-      </EmptyContent>
-    </Empty>
+          <Button onClick={() => setIsNewProjecOpen(true)} variant="outline">
+            Create project manually
+          </Button>
+        </EmptyContent>
+      </Empty>
+
+      <AIToolsModal
+        open={isToolsOpen}
+        onClose={() => setIsToolsOpen(false)}
+      />
+
+      <NewProjectDialog
+        open={isNewProjecOpen}
+        onClose={() => setIsNewProjecOpen(false)}
+      />
+    </>
   )
 }
